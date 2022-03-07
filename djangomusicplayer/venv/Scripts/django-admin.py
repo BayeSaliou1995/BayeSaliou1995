0d30874c92d54a5d5a0462798b88edb042a37979
Baye Saliou DIAW <diawbayesaliou463@gmail.com>
2022-03-07 21:14:28 +0000

    Dépot du projet MusicApp

---
+++ djangomusicplayer/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Baye Saliou DIAW\Documents\djangoCostumer\projetSiteDjango\Music_Streaming_App_Using_Django_Framework\djangomusicplayer\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
